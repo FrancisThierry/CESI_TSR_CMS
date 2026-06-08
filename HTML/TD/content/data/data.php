0c1e30bf30092d9911ed8b2f7afcd8fb536da110
FrancisThierry <boulangerthie@gmail.com>
2026-06-08 14:07:47 +0200

    add array

---
+++ HTML/TD/content/data/data.php
@@ -0,0 +1,20 @@
+<?php
+
+$marjolaine = array(
+    "title" => "La marjolaine en détails",
+    "firstparagraph" => "La marjolaine ou origan des jardins (Origanum majorana) est une espèce de plantes à fleurs de la famille des Lamiacées. C'est une plante herbacée vivace originaire du bassin méditerranéen et cultivée ailleurs comme plante condimentaire pour ses feuilles aromatiques. C'est une espèce très proche de l'origan commun (Origanum vulgare).",
+    "secondparagraph" => "C'est une plante vivace, de 60 cm de haut.
+Les feuilles sont opposées, duveteuses, vert grisâtre, de forme ovale entière, de 1 à 2 cm de long. Les petites fleurs sont blanches ou mauves, réunies en groupes serrés à l'aisselle des feuilles avec deux bractées en forme de cuillère.",
+    "image" => "./assets/images/500px-Origanum_majorana.jpg",
+    "link" => "https://fr.wikipedia.org/wiki/Origanum_majorana"
+);
+
+$petunia = array(
+    "title" => "Le pétunia en détails",
+    "firstparagraph" => "",
+    "secondparagraph" => "",
+    "image" => "./assets/images/500px-Origanum_majorana.jpg",
+    "link" => "https://fr.wikipedia.org/wiki/Origanum_majorana"
+);
+
+$blog = array ( "marjolaine" => $marjolaine, "petunia" => $petunia);
--- HTML/TD/index.php
+++ HTML/TD/index.php
@@ -1,67 +1,45 @@
-<?php $title = "La marjolaine en php"; ?>
+<?php require_once("./content/data.php"); ?>
 
 <?php
-$numberarray = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-//accès à un index particulier
-echo $numberarray[0];
+$flower = $_GET["flower"];
+$color = $_GET["color"];
 
+echo htmlspecialchars( "le parametre flower est $flower");
 
-
-//boucle for
-for ($i = 0; $i < count($numberarray); $i++) {
-    echo $numberarray[$i];
-}
-
-//un tableau associatif
-$numberarray = array(
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "four" => 4,
-    "five" => 5,
-    "six" => 6,
-    "seven" => 7,
-    "eight" => 8,
-    "nine" => 9,
-    "ten" => 10,
-);
-
-//accès à un index particulier
-echo $numberarray["one"];
 
 ?>
 
 <! doctype html>
     <html lang="fr">
-
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="./assets/css/style.css">
         <title>La marjolaine</title>
     </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="./index.php?flower=marjolaine&color=orange">Marjolaine</a></li>
+                <li><a href="./index.php?flower=petunia">Pétunia</a></li>
+                <li><a href="./index.php?flower=coquelicot">Coquelicot</a></li>
+            </ul>
+        </nav>
+        <h1><?php echo $blog["title"]; ?></h1>
+        <p class="enorange"><?= $blog["firstparagraph"]; ?></p>
+        <p><?= $blog["secondparagraph"]; ?></p>
 
-    <body >
-        <h1><?php echo $title; ?></h1>
-        <p class="enorange">La marjolaine ou origan des jardins (Origanum majorana) est une espèce de plantes à fleurs de la famille des
-            Lamiacées. C'est une plante herbacée vivace originaire du bassin méditerranéen et cultivée ailleurs comme
-            plante condimentaire pour ses feuilles aromatiques. C'est une espèce très proche de l'origan commun
-            (Origanum vulgare).</p>
-        <p>C'est une plante vivace, de 60 cm de haut. <br /> Les feuilles sont opposées, duveteuses, vert grisâtre, de
-            forme ovale entière, de 1 à 2 cm de long. Les petites fleurs sont blanches ou mauves, réunies en groupes
-            serrés à l'aisselle des feuilles avec deux bractées en forme de cuillère.</p>
+        <a href=<?= $blog["link"]; ?> target="_blank">En savoir plus</a>
 
-            <a href="https://fr.wikipedia.org/wiki/Origanum_majorana" target="_blank">En savoir plus</a>
-
-            <a href="./autrepage.html">Autre page</a>
+        <a href="./autrepage.html">Autre page</a>
 
 
 
 
         <p>
-              <h1 class="enorange">En image</h1>
+        <h1 class="enorange">En image</h1>
 
-            <img src="./assets/images/500px-Origanum_majorana.jpg" alt="Origanum majorana" />
+        <img src=<?= $blog["image"]; ?> alt="Origanum majorana" title="Origanum majorana" />
         </p>
 
 
